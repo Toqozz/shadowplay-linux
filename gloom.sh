ee5f4cc4e025e07aec1579e414f846f9339e4d05
Michael Palmos <toqoz@hotmail.com>
2016-10-08 14:23:52 +1000

    runs better now!

--- gloom.sh
+++ gloom.sh
@@ -4,11 +4,12 @@ set -eu
 
 INC=0
 HOME=$HOME
-INSTALL="ffmpeg"
+INSTALL="$HOME/opt/ffmpeg/bin/ffmpeg"
 
 
 function gloom {
-    ${INSTALL} -f pulse -i 0 -f x11grab -r 30 -s 2560x1440 -i :0.0 -vcodec h264_nvenc -preset default -gpu 0 -bit_rate 5000 -profile:v main -acodec aac -pix_fmt nv12 /tmp/out.mp4 &&
+    ${INSTALL} -f pulse -i 0 -f x11grab -framerate 30 -s 2560x1440 -i :0.0 -c:v h264_nvenc -preset:v llhq -profile:v high -pix_fmt nv12 -b:v 15M -acodec aac /tmp/out.mp4 &&
+    #${INSTALL} -f pulse -i 0 -f x11grab -r 30 -s 2560x1440 -i :0.0 -vcodec h264_nvenc -preset default -gpu 0 -bit_rate 30000 -profile:v high444p -acodec aac -pix_fmt nv12 /tmp/out.mp4 &&
 
     FILE="${HOME}/Videos/gloom-${INC}.mp4"
 
@@ -34,7 +35,7 @@ function leave {
 }
 
 while true; do
-    gloom
-    clean
+    gloom &&
+    clean &&
     leave
 done
